7934fde6db35650b498fd1cb2172373a2e5ab33a
Carlos Garcia Campos <carlosgc@gnome.org>
2017-08-09 14:24:22 +0200

    release.sh: Include generated documentation in the tarball

--- mesonscripts/release.sh
+++ mesonscripts/release.sh
@@ -4,6 +4,7 @@ NAME="$1"
 VERSION="$2"
 
 test -n "${MESON_SOURCE_ROOT}" || exit 1
+test -n "${MESON_BUILD_ROOT}" || exit 1
 test -n "${NAME}" || exit 1
 test -n "${VERSION}" || exit 1
 
@@ -15,6 +16,11 @@ rm -f "${NAME}-${VERSION}.tar.xz"
 
 echo "Creating git archive…"
 git archive --prefix="${NAME}-${VERSION}/" --format=tar HEAD -o ${NAME}-${VERSION}.tar
+
+echo "Adding documentation to archive…"
+BUILD_DIR=$(realpath --relative-to=${MESON_SOURCE_ROOT} ${MESON_BUILD_ROOT})
+tar --transform "s|${BUILD_DIR}|${NAME}-${VERSION}|" -uf ${NAME}-${VERSION}.tar ${BUILD_DIR}/docs/reference/html
+tar --transform "s|${BUILD_DIR}|${NAME}-${VERSION}|" -uf ${NAME}-${VERSION}.tar ${BUILD_DIR}/docs/tools/*.1
 
 echo "Compressing archive…"
 xz -f "${NAME}-${VERSION}.tar"
